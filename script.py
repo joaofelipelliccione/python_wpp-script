a5e666b45f0bdd0c113ff67ed362715160365be0
João Felipe P <jf.pelliccione@gmail.com>
2022-06-04 18:06:29 -0300

    Melhorando script!

--- script.py
+++ script.py
@@ -21,18 +21,25 @@ while len(browser.find_elements_by_id('side')) < 1:
 """
 Código que envia as mensagens para os respectivos contatos.
 """
+already_sent_array = []
+
 for i, message in i_messages:
     contact_name = contact_list['Nome do Contato'][i]
     cellphone_num = contact_list['Número'][i]
     final_message = f'Olá {contact_name},\n{message}'
     encoded_message = urllib.parse.quote(final_message)
+
+    send_btn_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'
     link_wpp = f'https://web.whatsapp.com/send?phone={cellphone_num}&text={encoded_message}'
 
-    print(f'Enviando mensagem p/ {contact_name}')
     browser.get(link_wpp)
-    while len(browser.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')) < 1:
+    while len(browser.find_elements_by_xpath(send_btn_xpath)) < 1:
         time.sleep(1)
 
-    send_btn = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')
+    send_btn = browser.find_element_by_xpath(send_btn_xpath)
     send_btn.click()
+    already_sent_array.append([contact_name, cellphone_num])
+
+    already_sent_df = pd.DataFrame(already_sent_array, columns=['Nome do Contato', 'Número'])
+    already_sent_df.to_csv('already_sent_list.csv', index=False)
     time.sleep(10)
